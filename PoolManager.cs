0b047bcafdc1d766c018ed17ed79883532b21868
VitaliySunstory <shvetsovvitaliy06@gmail.com>
2017-02-14 00:04:13 +0200

    Added Pooling System

---
+++ PoolManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolManager : MonoBehaviour {
+    static PoolManager instance;
+
+    //in enum order
+    public GameObject[] weaponPrefabs;
+    public GameObject[] superWeaponPrefabs;
+    public GameObject meteorPrefab;
+
+    public GameObject explosionPrefab;
+    public GameObject lineRendererPrefab;
+    public GameObject bananaEmitter;
+
+    public GameObject[] otherPrefabs;
+    public GameObject starsPrefab;
+    public GameObject tankFirePrefab;
+    public GameObject treasurePrefab;
+    public GameObject confettiPrefab;
+
+    public GameObject damageTextPrefab;
+
+    public Transform poolHolder;
+
+    public GameObject poisonHitPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        for(int i=0; i < weaponPrefabs.Length; i++)
+        {
+            PoolingSystem.Prespawn(weaponPrefabs[i], 1, poolHolder);
+        }
+
+        for(int i=0; i < superWeaponPrefabs.Length; i++)
+        {
+            PoolingSystem.Prespawn(superWeaponPrefabs[i], 1, poolHolder);
+        }
+
+        PoolingSystem.Prespawn(bananaEmitter, 6, poolHolder);
+        PoolingSystem.Prespawn(meteorPrefab, 19, poolHolder);
+        PoolingSystem.Prespawn(explosionPrefab, 19, poolHolder);
+        PoolingSystem.Prespawn(lineRendererPrefab, 1, poolHolder);
+        PoolingSystem.Prespawn(tankFirePrefab, 1, poolHolder);
+        PoolingSystem.Prespawn(poisonHitPrefab, 3, poolHolder);
+        PoolingSystem.Prespawn(treasurePrefab, 2, poolHolder);
+        PoolingSystem.Prespawn(starsPrefab, 2, poolHolder);
+        PoolingSystem.Prespawn(confettiPrefab, 1, poolHolder);
+
+        PoolingSystem.Prespawn(damageTextPrefab, 10, poolHolder);
+
+        for (int i = 0; i < otherPrefabs.Length; i++)
+            PoolingSystem.Prespawn(otherPrefabs[i], 2, poolHolder);
+
+        CameraController.INSTANCE.gameIsStarted = true;
+    }
+
+    public GameObject GetWeaponPrefab(WeaponType type)
+    {
+        return weaponPrefabs[(int)type];
+    }
+
+    public GameObject GetWeaponPrefab(SuperWeaponType type)
+    {
+        return superWeaponPrefabs[(int)type];
+    }
+
+    public GameObject GetTankFirePrefab()
+    {
+        return tankFirePrefab;
+    }
+
+    public GameObject GetDamageTextPrefab()
+    {
+        return damageTextPrefab;
+    }
+
+    static public PoolManager INSTANCE
+    {
+        get { return instance; }
+    }
+}
---
+++ PoolingSystem.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+interface IPoolableComponent
+{
+    void Spawned();
+    void Despawned();
+}
+
+public static class PoolingSystem {
+    static Dictionary<GameObject, PrefabPool> mainPool;
+    static Dictionary<GameObject, PrefabPool> _goToMainPool;
+    public static Transform poolHolder;
+
+    static PoolingSystem()
+    {
+        mainPool = new Dictionary<GameObject, PrefabPool>();
+        _goToMainPool = new Dictionary<GameObject, PrefabPool>();
+    }
+    public static void ClearPools()
+    {
+        mainPool.Clear();
+        _goToMainPool.Clear();
+    }
+
+
+    static public GameObject Spawn(GameObject objPrefab, Vector3 position)
+    {
+        if (!mainPool.ContainsKey(objPrefab))
+        {
+            mainPool.Add(objPrefab, new PrefabPool());
+        }
+        GameObject createdObj = mainPool[objPrefab].Spawn(objPrefab, position);
+        _goToMainPool.Add(createdObj, mainPool[objPrefab]);
+
+        return createdObj;
+    }
+
+    static public bool Despawn(GameObject obj)
+    {
+        if (!_goToMainPool.ContainsKey(obj))
+        {
+            Debug.LogError("POOL ERROR");
+            return false;
+        }
+        PrefabPool pool = _goToMainPool[obj];
+        if (pool.Despawn(obj))
+        {
+            _goToMainPool.Remove(obj);
+        }
+
+        return false;
+    }
+
+    static public void Prespawn(GameObject obj, int size, Transform poolHolder)
+    {
+        List<GameObject> spawned = new List<GameObject>();
+        PoolingSystem.poolHolder = poolHolder;
+
+        for (int i = 0; i < size; i++)
+            spawned.Add(Spawn(obj, Vector3.up));
+
+        for (int i = 0; i < size; i++)
+            Despawn(spawned[i]);
+
+        spawned.Clear();
+    }
+}
+
+struct PoolablePrefabData
+{
+    public GameObject gameObject;
+    public IPoolableComponent[] poolableComponents;
+}
+
+class PrefabPool
+{
+    Dictionary<GameObject, PoolablePrefabData> activeList;
+    Queue<PoolablePrefabData> inActiveList;
+
+    public PrefabPool()
+    {
+        activeList = new Dictionary<GameObject, PoolablePrefabData>();
+        inActiveList = new Queue<PoolablePrefabData>();
+    }
+
+    public GameObject Spawn(GameObject obj, Vector3 position)
+    {
+        PoolablePrefabData data;
+        if (inActiveList.Count >= 1)
+        {
+            data = inActiveList.Dequeue();
+        }
+        else
+        {
+            GameObject go = GameObject.Instantiate(obj);
+            go.transform.SetParent(PoolingSystem.poolHolder, false);
+            data = new PoolablePrefabData();
+            data.gameObject = go;
+            data.poolableComponents = go.GetComponents<IPoolableComponent>();
+        }
+
+        if (data.gameObject != null)
+        {
+            data.gameObject.SetActive(true);
+            data.gameObject.transform.position = position;
+        }
+
+        if (PoolManagerNet.gameStarted)
+        {
+            foreach (var x in data.poolableComponents)
+            {
+                x.Spawned();
+            }
+        }
+
+        activeList.Add(data.gameObject, data);
+
+        return data.gameObject;
+    }
+
+    public bool Despawn(GameObject obj)
+    {
+        PoolablePrefabData data;
+        if (!activeList.ContainsKey(obj))
+        {
+            Debug.LogError("pool error");
+            return false;
+        }
+
+        data = activeList[obj];
+
+        if (PoolManagerNet.gameStarted)
+        {
+           foreach (var x in data.poolableComponents)
+            {
+                x.Despawned();
+            }
+        }
+
+        if (data.gameObject != null)
+           data.gameObject.SetActive(false);
+
+        activeList.Remove(data.gameObject);
+        inActiveList.Enqueue(data);
+        return true;
+    }
+}
